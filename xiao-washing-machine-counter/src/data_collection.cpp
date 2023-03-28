30844ebd120130fb0979e753688a91c2ecd1ca43
gigwegbe <gigwegbe@gmail.com>
2023-03-28 08:57:24 +0100

    Added basic data collection script

---
+++ xiao-washing-machine-counter/src/data_collection.cpp
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <MPU6050.h>
+// #include <SD.h>
+#include <SPI.h>
+
+#define FREQUENCY_HZ        100
+#define INTERVAL_MS         (1000 / (FREQUENCY_HZ + 1))
+
+MPU6050 mpu;
+
+
+static unsigned long last_interval_ms = 0;
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
+}
+
+void loop()
+{
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  if (millis() > last_interval_ms + INTERVAL_MS){
+  last_interval_ms = millis(); 
+  Serial.print(last_interval_ms); Serial.print("\t");
+  Serial.print(normAccel.XAxis); Serial.print("\t");
+  Serial.print(normAccel.YAxis); Serial.print("\t");
+  Serial.println(normAccel.ZAxis);
+  }
+}
