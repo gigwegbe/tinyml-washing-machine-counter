9186dddf1d1cc91d5f24b629cdf58465c2fd12fc
gigwegbe <gigwegbe@gmail.com>
2023-03-28 11:33:09 +0100

    Added save to sd feature

--- xiao-washing-machine-counter/src/data_collection.cpp
+++ xiao-washing-machine-counter/src/data_collection.cpp
@@ -1,40 +1,88 @@
+#include <SD.h>
 #include <Wire.h>
 #include <MPU6050.h>
-// #include <SD.h>
 #include <SPI.h>
 
-#define FREQUENCY_HZ        100
-#define INTERVAL_MS         (1000 / (FREQUENCY_HZ + 1))
+#define FREQUENCY_HZ 100
+#define INTERVAL_MS (1000 / (FREQUENCY_HZ + 1))
+#define sensorPin 0
 
 MPU6050 mpu;
+Sd2Card card;
 
-static unsigned long data_collection_interval_ms = 0;
+static unsigned long data_collection_interval_ms = 6000;
 static unsigned long last_interval_ms = 0;
+const int chipSelect = 2;
 
-void setup() 
+void setup()
 {
+  pinMode(sensorPin, INPUT);
   Serial.begin(115200);
 
   Serial.println("Initialize MPU6050");
 
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
   {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
 
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  // SD.begin();
+  SD.begin(chipSelect);
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect))
+  {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1)
+      ;
+  }
+  Serial.println("card initialized.");
 }
 
 void loop()
 {
-  while(last_interval_ms < data_collection_interval_ms){
-        Vector normAccel = mpu.readNormalizeAccel();
-        if (millis() > last_interval_ms + INTERVAL_MS){
-            last_interval_ms = millis(); 
-            Serial.print(last_interval_ms); Serial.print("\t");
-            Serial.print(normAccel.XAxis); Serial.print("\t");
-            Serial.print(normAccel.YAxis); Serial.print("\t");
-            Serial.println(normAccel.ZAxis);
+  Vector normAccel = mpu.readNormalizeAccel();
+  // Log current data
+  String dataString = "";
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("log012.txt", FILE_WRITE);
+
+  if (last_interval_ms < data_collection_interval_ms)
+  {
+    if (millis() > last_interval_ms + INTERVAL_MS)
+    {
+
+      // Log current data
+      dataString += last_interval_ms;
+      dataString += ",";
+      dataString += String(normAccel.XAxis);
+      dataString += ",";
+      dataString += String(normAccel.YAxis);
+      dataString += ",";
+      dataString += String(normAccel.ZAxis);
+
+      // if the file is available, write to it:
+      if (dataFile)
+      {
+        last_interval_ms = millis();
+        dataFile.println(dataString);
+        dataFile.close();
+        // print to the serial port too:
+        Serial.println(dataString);
       }
+      // if the file isn't open, pop up an error:
+      else
+      {
+        Serial.println("error opening datalog.txt");
+      }
+    }
   }
 }
